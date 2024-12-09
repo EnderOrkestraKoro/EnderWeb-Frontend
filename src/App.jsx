b264cdd168cd9353c7b8dbf845b6dcf747ba9234
Miray Sönmez <113294105+chiturca@users.noreply.github.com>
2024-12-09 22:00:30 +0300

    Build: react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import LoginPage from "./components/LoginPage";
+import HomePage from "./components/HomePage";
+import RepertoirePage from "./components/RepertoirePage";
+import ProfilePage from "./components/ProfilePage";
 
 function App() {
   return (
     <>
-      <button className="text-3xl font-bold underline">Ellow</button>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/repertoire" element={<RepertoirePage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+        </Routes>
+      </Router>
     </>
   );
 }
